8d485cd1e94316497c475b37dd9a138a61a9376a
adamtaher <adam.taher.2021@uni.strath.ac.uk>
2023-02-22 21:17:52 +0300

    I added class
    I added class for Profiles with setter and getter methods and private data members

--- ProfileHeader.h
+++ ProfileHeader.h
@@ -1,2 +1,29 @@
 #pragma once
-ffdivbj
+class Profiles {
+
+	string getName();
+	void setName(string);
+	String getemail_address();
+	void setemail_address(string);
+	string getUser_name();
+	void setUser_name(string);
+	int getdob_day();
+	void setdob_day(int );
+	int getdob_month();
+	void setdob_month(int);
+	int getdob_year();
+	void setdob_year(int);
+	char getPassword();
+	void setPassword(char);
+
+
+
+private:
+	string name;
+	string email_address;
+	string user_name;
+	int dob_day;
+	int dob_month;
+	int dob_year;
+	char password;
+};
