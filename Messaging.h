de7de22f4fa3053c3fbebdc0d60ce807e4c0cea1
BilalZah <bilal.zahid.2021@uni.strath.ac.uk>
2023-02-28 14:52:59 +0000

    Update Messaging.h
    added like button and number of likes

--- Messaging.h
+++ Messaging.h
@@ -3,9 +3,15 @@ class Messaging :public Profiles,public follow_list {
 public:
 	string getcomments();
 	void setcomments(string);
+	int getnumber_of_likes();
+	void setnumber_of_likes(int);
+	char getlike_button();
+	void setlike_button(char);
 
 
 
 private:
 	string comment;
+	int number_of_likes;
+	char like_button;
 };
