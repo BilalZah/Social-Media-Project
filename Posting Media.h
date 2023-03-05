3b70218ffdbab561398b84df7a80d1bf3dd78388
Bilal <bilal.zahid.2021@uni.strath.ac.uk>
2023-03-05 13:14:19 +0000

    Update Posting Media.h
    added share to followers

--- Posting Media.h
+++ Posting Media.h
@@ -3,6 +3,9 @@ class Posting Media:public Profiles, public follow_list {
 public:
 	int getnum_likes();
 	void setnum_likes(int);
+	char share_to_follow_list();
+	void share_to_follow_list (char)
 private:
 	int num_likes;
+	char share_to_follow_list;
 };
