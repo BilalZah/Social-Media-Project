3c6d7d78cbfb3978a39c6453e2d010f58b4231b1
Bilal <bilal.zahid.2021@uni.strath.ac.uk>
2023-03-06 10:44:37 +0000

    Added.cpp file
    added .cpp file for posting media

---
+++ Posting_Media.cpp
@@ -0,0 +1,14 @@
+#include "Profiles"
+#include "Posting_Media"
+#include "follow_list"
+using namespace std;
+#include <string>
+Posting_Media::Posting_Media(){}
+
+Posting_Media::~Posting_Media(){}
+int Posting_Media::getnum_likes(){return num_likes;}
+void Posting_Media::setnum_likes(int num_likes){this->num_likes = num_likes;}
+string Posting_Media::getcomments(){return comments}
+void Posting_Media::setcomments(string comments){this->comments = comments;}
+char Posting_Media::getshare_to_follow_list(return share_to_follow_list){}
+void Posting_Media::setshare_to_follow_list(char share_to_follow_list){this->share_to_follow_list = share_to_follow_list;}
