8ecde4bef88e89973c2a54d1b9a53c960098b21d
BilalZah <bilal.zahid.2021@uni.strath.ac.uk>
2023-03-07 13:46:59 +0000

    error fixing
    fixed errors in posting media files

--- Posting_Media.cpp
+++ Posting_Media.cpp
@@ -1,6 +1,6 @@
-#include "Profiles"
-#include "Posting_Media"
-#include "follow_list"
+#include "Profiles.h"
+#include "Posting_Media.h"
+#include "follow_list.h"
 using namespace std;
 #include <string>
 Posting_Media::Posting_Media(){}
@@ -8,9 +8,9 @@ Posting_Media::Posting_Media(){}
 Posting_Media::~Posting_Media(){}
 int Posting_Media::getnum_likes(){return num_likes;}
 void Posting_Media::setnum_likes(int num_likes){this->num_likes = num_likes;}
-string Posting_Media::getcomments(){return comments}
+string Posting_Media::getcomments() { return comments; }
 void Posting_Media::setcomments(string comments){this->comments = comments;}
-char Posting_Media::getshare_to_follow_list(return share_to_follow_list){}
+char Posting_Media::getshare_to_follow_list() { return share_to_follow_list; }
 void Posting_Media::setshare_to_follow_list(char share_to_follow_list){this->share_to_follow_list = share_to_follow_list;}
 string Posting_Media::getcaption(){return caption;}
 void Posting_Media::setcaption(string caption){this->caption=caption;}
--- Posting_Media.h
+++ Posting_Media.h
@@ -1,10 +1,12 @@
 #pragma once
-class Posting_Media:public Profiles, public follow_list {
+#include<string>
+using namespace std;
+class Posting_Media:public Profiles {
 public:
 	int getnum_likes();
 	void setnum_likes(int);
-	char share_to_follow_list();
-	void share_to_follow_list (char)
+	char getshare_to_follow_list();
+	void setshare_to_follow_list(char);
 	string getcomments();
 	void setcomments(string);
 	string getcaption();
@@ -13,5 +15,5 @@ private:
 	int num_likes;
 	char share_to_follow_list;
 	string comments;
-	string caption
+	string caption;
 };
