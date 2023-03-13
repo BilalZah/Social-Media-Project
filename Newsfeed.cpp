2df899f6ceb14e8bf126f116f3f257bb27284842
adamtaher <adam.taher.2021@uni.strath.ac.uk>
2023-03-13 23:40:57 +0300

    Added newsfeed cpp file

---
+++ Newsfeed.cpp
@@ -0,0 +1,14 @@
+#include"C:\Users\Dell\Documents\GitHub\Social-Media-Project/Newsfeed.h"
+using namespace std;
+#include<string>
+Newsfeed::Newsfeed() {}
+Newsfeed::~Newsfeed() {}
+
+string Newsfeed::getPost() { return post; }
+void Newsfeed::setPost(string post) { this->post = post; }
+
+
+string Newsfeed::getPost_name() { return post; }
+void Newsfeed::setPost_name(string post_name) { this->post_name = post_name; }
+
+
