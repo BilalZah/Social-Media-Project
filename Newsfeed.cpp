f9508f2bd6516b47a35e488f29d671986bd4a349
adamtaher <125990338+adamtaher@users.noreply.github.com>
2023-03-14 19:01:51 +0000

    Newsfeed.cpp file

--- Newsfeed.cpp
+++ Newsfeed.cpp
@@ -1,14 +1,29 @@
 #include"C:\Users\Dell\Documents\GitHub\Social-Media-Project/Newsfeed.h"
+#include "C:\Users\Dell\Documents\GitHub\Social-Media-Project/follow_list.h"
+
 using namespace std;
+#include<vector>
 #include<string>
-Newsfeed::Newsfeed() {}
+
+
+#include<string>
+Newsfeed::Newsfeed():follow_list(){}
 Newsfeed::~Newsfeed() {}
+//m_posts.push_back("          ") 
+//vector<std::string> Newsfeed::getPot}
+void Newsfeed::addPost(const std::string& username, const std::string& content) {
+    // Add the post to m_posts vector
+    m_posts.push_back("@" + username + " " + content);
+}
 
-string Newsfeed::getPost() { return post; }
-void Newsfeed::setPost(string post) { this->post = post; }
+// Retrieve all posts from the newsfeed
+std::vector<std::string> Newsfeed::getPosts() {
+    return m_posts;
+}
 
 
-string Newsfeed::getPost_name() { return post; }
+
+string Newsfeed::getPost_name() { return post_name; }
 void Newsfeed::setPost_name(string post_name) { this->post_name = post_name; }
 
 
