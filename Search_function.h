bd43434f0f6e6701db85cabf15490e20c1125cf4
adamtaher <adam.taher.2021@uni.strath.ac.uk>
2023-04-25 17:14:42 +0300

    updated search function included following in search function

--- Search_function.h
+++ Search_function.h
@@ -7,13 +7,15 @@ struct User {
 	string username;
 	string location;
 	string Interests;
+	string following;
+
 
 };// structure that contains users name, location, interests and username.
 vector<User> users = {
-			{"Ahmed Ali", "Ali", "Glasgow", "Gym,Football"},
-			{"Cristiano Ronaldo", "Ronaldo", "Riyadh", "Football,Gym"},
-			{"Mona Ahmed", "Mona", "Vancouver", "Food,Yoga"},
-			{"Elon Musk", "Elon", "Texas", "Space,Tesla"},
+			{"Ahmed Ali", "Ali", "Glasgow", "Gym,Football","Following"},
+			{"Cristiano Ronaldo", "Ronaldo", "Riyadh", "Football,Gym","Following"},
+			{"Mona Ahmed", "Mona", "Vancouver", "Food,Yoga","Following"},
+			{"Elon Musk", "Elon", "Texas", "Space,Tesla","Following"},
 			{"UN", "UN", "New York", "Health,Education "},
 
 };// vector of User which contains the data for each user
@@ -24,6 +26,7 @@ for (User user : users) {//using for loop checks each User object in the collect
 // by calling find function 
 		if (user.name.find(query) != string::npos ||
 			user.username.find(query) != string::npos ||
+			user.following.find(query) != string::npos ||
 			user.Interests.find(query) != string::npos ||
             user.location.find(query) != string::npos) {
 			results.push_back(user);
